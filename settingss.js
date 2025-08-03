c1210612b10ff3abf519550037d2e113ca663f00
ALI-HAIDER715 <chmunirkhan51214@gmail.com>
2025-08-04 02:37:48 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~I6sTQbAZ#N8kVU8CpvyML-5k-WlUcQCZS1i6DkUPF4MH26ZbYBX8",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "923209026006", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
